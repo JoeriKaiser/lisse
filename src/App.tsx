e30e7b6320bb528fa0d5968374ce2b473332e6a9
JoeriKaiser <joeri.kaiser@hotmail.com>
2024-07-29 23:14:02 +0200

    Wrapped main with QueryClientProvider instead of App only

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import { QueryClientProvider } from 'react-query';
 import './App.css';
-import { QueryClient } from 'react-query';
 import ScannerContainer from './components/Scanner/ScannerContainer';
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <ScannerContainer />
-      </QueryClientProvider>
+      <ScannerContainer />
     </>
   );
 }
