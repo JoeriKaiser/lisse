af34c099527dce2eee0d3f9eb369bc60fd157dc2
JoeriKaiser <joeri.kaiser@hotmail.com>
2024-08-01 20:51:56 +0200

    Set user after login

--- src/features/Login/LoginContainer.tsx
+++ src/features/Login/LoginContainer.tsx
@@ -1,13 +1,15 @@
 import { useLogin } from '../../api/mutations/login';
+import { useAuth } from '../../context/AuthContext';
 import { IFormInputs } from './formTypes';
 import Login from './Login';
 
 const LoginContainer = () => {
+  const { setUser } = useAuth();
   const loginUser = useLogin();
   const onSubmit = (inputs: IFormInputs) => {
     loginUser.mutate(inputs, {
-      onSuccess: () => {
-        console.log('success');
+      onSuccess: (data) => {
+        setUser(data);
       },
       onError: () => {
         console.log('error');
