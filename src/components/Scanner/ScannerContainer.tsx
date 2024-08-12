2383f933348e4cbfdf5f7fd9affb93963efb62f3
JoeriKaiser <joeri.kaiser@hotmail.com>
2024-08-12 21:38:38 +0200

    Temp used setState

--- src/components/Scanner/ScannerContainer.tsx
+++ src/components/Scanner/ScannerContainer.tsx
@@ -19,6 +19,8 @@ const ScannerContainer = () => {
   };
 
   const sendScan = () => {
+    // TODO temp, clear items
+    setItems([]);
     createScan.mutate(testScan, {
       onSuccess: () => {
         console.log('success');
